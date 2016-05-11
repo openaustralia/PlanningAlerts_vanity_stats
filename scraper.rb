c24458f3034ecda8e89ab655c539e038dcf17d26
Luke Bacon <luke@equivalentideas.com>
2016-05-11 10:48:07 +1000

    Add basic scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,24 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
+def extract_number(element)
+  element.text.delete(",").to_i
+end
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'mechanize'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+agent = Mechanize.new
+
+# Read in a page
+page = agent.get("https://www.planningalerts.org.au")
+
+metrics_numbers = page.search("#stats-wrapper .number")
+metrics = {
+  date_scraped: Date.today.to_s,
+  applications_added_in_last_week: extract_number(metrics_numbers[0]),
+  email_alerts_sent_in_last_week: extract_number(metrics_numbers[1]),
+  new_alert_subscriptions_in_last_week: extract_number(metrics_numbers[2]),
+  new_comments_made_in_last_week: extract_number(metrics_numbers[3])
+}
+
+ScraperWiki.save_sqlite([:date_scraped], metrics)
